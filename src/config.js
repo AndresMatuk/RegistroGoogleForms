ab43cb843ff21ed83a9e9d3b7d66b457a991b340
AndresMatuk <andres.matuk@soy.sena.edu.co>
2024-06-22 12:43:07 -0500

    ajustes en la base de datos

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ export const PORT = process.env.PORT || 3000
 
 export const MYSQLHOST = process.env.MYSQLHOST || '127.0.0.1'
 export const MYSQLUSER = process.env.MYSQLUSER || 'root'
-export const MYSQLPASSWORD = process.env.MYSQLPASSWORD || ''
-export const MYSQLDATABASE = process.env.MYSQLDATABASE || 'apirest'
+export const MYSQLPASSWORD = process.env.MYSQLPASSWORD || 'andres123'
+export const MYSQLDATABASE = process.env.MYSQLDATABASE || 'formularioGoogle'
 export const MYSQLPORT = process.env.MYSQLPORT || 3306
